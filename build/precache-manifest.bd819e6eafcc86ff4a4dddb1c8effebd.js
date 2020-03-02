faab75f1597dcae69a9d6672a1cb17df7a4fd3e1
tymkowy <tymekjw@gmail.com>
2020-03-02 10:27:54 +0100

    Kalkulator V1 - HBC

---
+++ build/precache-manifest.bd819e6eafcc86ff4a4dddb1c8effebd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e975de7e2fda72861f29fa6ab816d1",
+    "url": "/kalkulator/index.html"
+  },
+  {
+    "revision": "ff7eed0643f869b118dc",
+    "url": "/kalkulator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4fb19482b6fb0bdaa574",
+    "url": "/kalkulator/static/css/main.efa1d33d.chunk.css"
+  },
+  {
+    "revision": "ff7eed0643f869b118dc",
+    "url": "/kalkulator/static/js/2.00e26318.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kalkulator/static/js/2.00e26318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb19482b6fb0bdaa574",
+    "url": "/kalkulator/static/js/main.c88a699d.chunk.js"
+  },
+  {
+    "revision": "8250f8e29a468761d043",
+    "url": "/kalkulator/static/js/runtime-main.68e4ee23.js"
+  },
+  {
+    "revision": "3f9eec3c27c0aa96733e11daafc7983d",
+    "url": "/kalkulator/static/media/bg-oferta.3f9eec3c.png"
+  },
+  {
+    "revision": "184cc0c39d3d08fdf9ae11345778f6ee",
+    "url": "/kalkulator/static/media/logo-hb.184cc0c3.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,190 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from './logo-hb.png'
+import Select from "react-select";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const diety = [1300, 1600, 1800, 2000, 2300, 2800]
+const regex = new RegExp(/^([1-9]|[0-9][.][0-9]|[1-9][.]|[1-9][.][0-9][0-9]|[1-9][0-9]|[1-9][0-9][.][0-9]|[1-9][0-9][0-9][.]|[1][0-9][0-9]|[1-9][0-9][.][0-9][0-9]|[1-9][1-9][.][0-9][0-9]|[1][0-9][0-9][.][0-9]|[1][0-9][0-9][.][0-9][0-9]|20[0-0])$/)
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            plec: {value: -161, label: 'Kobieta'},
+            wzrost: '',
+            waga: '',
+            wiek: '',
+            cel: {value: 0, label: 'Redukcja'},
+            aktywnosc: {value: 0, label: 'Niska'},
+            wynik: '',
+            dieta: '',
+            uzupelnijDane: false
+        }
+
+        this.changePlec = this.changePlec.bind(this)
+        this.changeWzrost = this.changeWzrost.bind(this)
+        this.changeWiek = this.changeWiek.bind(this)
+        this.changeWaga = this.changeWaga.bind(this)
+        this.changeCel = this.changeCel.bind(this)
+        this.changeAktywnosc = this.changeAktywnosc.bind(this)
+    }
+
+    changePlec(e) {
+        this.setState({plec: e})
+    }
+
+    changeWzrost(e) {
+        if (regex.test(e.target.value) || e.target.value === "") {
+            this.setState({wzrost: e.target.value})
+        }
+    }
+
+    changeWiek(e) {
+        if (regex.test(e.target.value) || e.target.value === "") {
+            this.setState({wiek: e.target.value})
+        }
+    }
+
+    changeWaga(e) {
+        if (regex.test(e.target.value) || e.target.value === "") {
+            this.setState({waga: e.target.value})
+        }
+    }
+
+    changeCel(e) {
+        this.setState({cel: e})
+    }
+
+    changeAktywnosc(e) {
+        this.setState({aktywnosc: e})
+    }
+
+    obliczZapotrzebowanie() {
+        const {plec, wzrost, waga, wiek, cel, aktywnosc} = this.state
+
+        if (Boolean(wzrost) && Boolean(waga) && Boolean(wiek)) {
+            let wynik = (wzrost * 6.25) + (waga * 9.99) - (wiek * 4.92)
+
+            wynik += plec.value
+            wynik += cel.value
+            wynik += aktywnosc.value
+            wynik = Math.ceil(wynik / 10) * 10
+
+            let dieta = this.najblizszaDieta(wynik)
+
+            this.setState({wynik: wynik + ' kcal', dieta: dieta, uzupelnijDane: false})
+        } else {
+            this.setState({uzupelnijDane: true})
+        }
+    }
+
+    najblizszaDieta(wynik) {
+        let closest = null;
+        if (this.state.cel.label === 'Utrzymanie wagi'){
+            for (let i=0; i<diety.length; i++){
+                if (wynik < diety[i]){
+                    closest = diety[i-1];
+                    break;
+                }
+            }
+        } else {
+            closest = diety.reduce((prev, curr) => {
+                return (Math.abs(curr - wynik) < Math.abs(prev - wynik) ? curr : prev);
+            });
+        }
+
+        console.log(closest)
+
+        switch (closest) {
+            case 1300:
+                return closest + ' kcal BASIC / WEGE';
+            case 1600:
+                return closest + ' kcal BASIC / WEGE';
+            case 1800:
+                return closest + ' kcal BASIC / WEGE / GYM';
+            case 2000:
+                return closest + ' kcal BASIC / WEGE';
+            case 2300:
+                return closest + ' kcal BASIC / WEGE';
+            case 2800:
+                return closest + ' kcal GYM';
+        }
+    }
+
+    render() {
+
+        const {plec, wzrost, waga, wiek, cel, aktywnosc, wynik, dieta, uzupelnijDane} = this.state
+
+        return <div className="main">
+            <div className="logo">
+                <img src={logo} className={'img-fluid'}/>
+            </div>
+            <div className="bmr-calc">
+
+                <label>Płeć:</label>
+                <Select
+                    onChange={this.changePlec}
+                    defaultValue={plec}
+                    options={[
+                        {value: -161, label: 'Kobieta'},
+                        {value: 5, label: 'Mężczyzna'},
+                    ]}
+                />
+
+                <label>Wiek:</label>
+                <input className={'form-control'} value={wiek} onChange={this.changeWiek}/>
+
+                <label>Waga:</label>
+                <input className={'form-control'} value={waga} onChange={this.changeWaga}/>
+
+                <label>Wzrost:</label>
+                <input className={'form-control'} value={wzrost} onChange={this.changeWzrost}/>
+
+                <label>Aktywność fizyczna:</label>
+                <Select
+                    onChange={this.changeAktywnosc}
+                    defaultValue={aktywnosc}
+                    options={[
+                        {value: 0, label: 'Niska'},
+                        {value: 300, label: 'Średnia'},
+                        {value: 600, label: 'Wysoka'},
+                    ]}
+                />
+
+                <label>Cel:</label>
+                <Select
+                    onChange={this.changeCel}
+                    defaultValue={cel}
+                    options={[
+                        {value: 0, label: 'Redukcja'},
+                        {value: 300, label: 'Utrzymanie wagi'},
+                        {value: 600, label: 'Masa'},
+                    ]}
+                />
+
+                <div>
+                    <button className={'btn btn-info'} onClick={() => this.obliczZapotrzebowanie()}>Oblicz
+                        zapotrzebowanie
+                    </button>
+                    {uzupelnijDane ? <div className={'uzupelnijDane'}>Uzupełnij dane</div> :
+                        <>
+                            <div className={'wynik'}>{wynik !== '' ?
+                                <div>Twoje zapotrzebowanie to: <div className={'font-weight-bolder'}>{wynik}</div>
+                                </div> : null}</div>
+                            <div className={'dieta'}>{dieta !== '' ?
+                                <div>Proponowana dieta: <div className={'font-weight-bolder'}>{dieta}</div>
+                                </div> : null}</div>
+                            <div>
+                                <small>Wyliczenia z powyższego kalkulatora są wyliczeniami orientacyjnymi, nie należy na nich bazować w 100%.</small>
+                            </div>
+                        </>}
+                </div>
+
+            </div>
+        </div>
+    }
+
 }
-
-export default App;
